311f5068ef7a1d60e18311aaa9dd4f791255eaf9
Bruno Lusa <bruno.lusa@gmail.com>
2020-08-06 21:10:10 -0300

    Inserindo teste da atualização de carros existentes

--- src/main/java/utils/StaticValues.java
+++ src/main/java/utils/StaticValues.java
@@ -4,4 +4,5 @@ public class StaticValues {
 
     public static final String ADD_SUCCESS = "Novo carro adicionado com sucesso!";
     public static final String DELETE_SUCCESS = "Carro deletado com sucesso.";
+    public static final String UPDATE_SUCCESS = "Carro atualizado com sucesso!";
 }
---
+++ src/test/java/test/carros/AtualizarCarroTest.java
@@ -0,0 +1,25 @@
+package test.carros;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pageObjects.CarroPage;
+import utils.BaseTest;
+
+import static utils.StaticValues.DELETE_SUCCESS;
+import static utils.StaticValues.UPDATE_SUCCESS;
+
+public class AtualizarCarroTest extends BaseTest {
+
+    @Test
+    public void deveAtualizarCarroExistente(){
+        CarroPage carroPage = new CarroPage(driver);
+
+        carroPage.atualizar().inserirMarca("Nova marca").inserirModelo("Novo modelo").gravar();
+
+        String resultado = carroPage.resultadoInclusaoCarro();
+
+        Assert.assertEquals(resultado, UPDATE_SUCCESS);
+
+
+    }
+}
